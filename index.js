6090066ef154c737846e2dd7f57b651dcea06f28
sadek2021 <sadek102buft@gmail.com>
2021-11-01 19:10:09 +0600

    update

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -18,6 +19,8 @@ async function run() {
         await client.connect();
         const database = client.db('travel_time');
         const packageCollection = database.collection('packages');
+        const destinationCollection = database.collection('destinations');
+        const bookingsCollection = database.collection('bookings');
 
         // Get API
         app.get('/packages', async (req, res) => {
@@ -37,11 +40,56 @@ async function run() {
 
         })
 
+        // Post Booking API
+        app.post('/bookings', async (req, res)=>{
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.json(result);
+        })
+
+        // Delete Bookings API
+        app.delete('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookingsCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // Status Update API
+        app.put('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const updatedStatus = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set: {
+                    status: updatedStatus.status
+                }
+            };
+            const result = await bookingsCollection.updateOne(filter, updateDoc, options)
+            console.log('update', req.body);
+            res.json(result)
+        })
+
         // Get Packages API
         app.get('/packages', async (req, res) => {
             const cursor = packageCollection.find({});
             const packages = await cursor.toArray();
             res.send(packages)
+        })
+
+        // Get Booking API
+        app.get('/bookings', async(req, res) =>{
+            const cursor = bookingsCollection.find({});
+            const bookings = await cursor.toArray();
+            res.send(bookings)
+        })
+
+        // Get Destination API
+        app.get('/destinations', async(req, res)=>{
+            const cursor = destinationCollection.find({});
+            const destinations = await cursor.toArray();
+            res.json(destinations)
         })
 
     }
